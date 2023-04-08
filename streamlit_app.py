12de0024222b5b756e9de11c1089a6fd9d12a3f2
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-04-08 22:04:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,54 @@ st.set_page_config(page_title='Youtube Analysis',
 
 st.sidebar.header('Input her')
 user_input = st.sidebar.text_input("Enter the channel code:")
+
+
+api_key ="AIzaSyA8gdudsfXk1YcEDS42CyeSiD7zXA1KWhI"
+youtube = build('youtube','v3',developerKey=api_key)
+
+def get_video_id(youtube,playlistId):
+    request = youtube.playlistItems().list(part="contentDetails",playlistId=playlistId,maxResults=50)
+    response = request.execute()
+    video_ids=[]
+    for i in (response['items']):
+        video_ids.append(i['contentDetails']['videoId'])
+    next_page_token=response.get('nextPageToken')
+    more_pages=True
+    while more_pages:
+        if next_page_token is None:
+            more_pages=False
+        else:
+            request = youtube.playlistItems().list(
+                part='contentDetails',
+                playlistId =playlistId,
+                maxResults=50,
+                pageToken=next_page_token)
+            response=request.execute()
+            for i in range(len(response['items'])):
+                video_ids.append(response['items'][i]['contentDetails']['videoId'])
+            next_page_token=response.get('nextPageToken')
+    return video_ids
+  
+d=get_video_id(youtube,playlistId)
+
+def get_video_topic(youtube,d):
+    video_data=[]
+    for j in range(0,len(d),50):
+        request = youtube.videos().list(part="snippet,statistics,topicDetails",id=",".join(d[j:j+50]))
+        response = request.execute()
+        for i in response['items']:
+            data=dict(Tilte=i['snippet']['title'],Publishedat=i['snippet']['publishedAt'],Views=i['statistics']['viewCount'],like=i['statistics']['likeCount'] if 'likeCount' in i['statistics'] else 0 , comment=i['statistics']['commentCount'] if 'commentCount' in i['statistics'] else 0 , topic=i['topicDetails']['topicCategories'] if 'topicDetails' in i else None  )
+            video_data.append(data)
+    return video_data
+  
+whole_data=get_video_topic(youtube,d)
+whole_data=pd.DataFrame(whole_data)
+  
+whole_data['Views']=pd.to_numeric(whole_data['Views'])
+whole_data['like']=pd.to_numeric(whole_data['like'])
+whole_data['comment']=pd.to_numeric(whole_data['comment'])
+extra_date=whole_data['Publishedat']
+whole_data['Publishedat']=pd.to_datetime(whole_data['Publishedat'], format= '%Y/%m/%d')
+
+st.dataframe(whole_data)
+
