fb6cc6c4e73223e1d4d010b8f5ad09c30328f819
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-04-08 21:43:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ st.set_page_config(page_title='Youtube Analysis',
                    page_icon=':bar_chart:',
                    layout='wide')
 
+st.sidebar.header('Input her')
+user_input = st.text_input("Enter your name:")
