b485779842c0a060c8ee1cff462df70ed36bfa5f
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-04-08 22:22:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,20 @@ user_input = st.sidebar.text_input("Enter the channel code:")
 if user_input:
   api_key ="AIzaSyA8gdudsfXk1YcEDS42CyeSiD7zXA1KWhI"
   youtube = build('youtube','v3',developerKey=api_key)
+  
+  
+  def get_channel_stats(youtube,channel_id):
+    request = youtube.channels().list(part="snippet,contentDetails,statistics",id=channel_id)
+    response = request.execute()
+    return response
+  data=get_channel_stats(youtube,user_input)
+  data_dict=[]
+  for i in data['items']:
+    data_dict.append(dict(Channel_Name=i['snippet']['title'],Description=i['snippet']['description'],subcribers=i['statistics']['subscriberCount'],videoCount=i['statistics']['videoCount'],viewCount=i['statistics']['viewCount'],uploads=i['contentDetails']['relatedPlaylists']['uploads']))
+  df=pd.DataFrame(data_dict)
+  playlistId=df.iloc[0:,[0,5]].loc[df['Channel_Name']=='MrBeast','uploads']
+  playlistId=playlistId.reset_index()
+  playlistId=playlistId.iloc[0,1]
 
   def get_video_id(youtube,playlistId):
     request = youtube.playlistItems().list(part="contentDetails",playlistId=playlistId,maxResults=50)
