fa79033069330ec94e8e3852422caeea4767e50d
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-04-11 21:13:48 +0530

    Update view page.py

--- view page.py
+++ view page.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+
+class Multiapp():
+  def __init__(self):
+    self.apps=[]
+    
+  def add_app(self,title,function):
+    self.apps.append({'title':title,'func':function})
+    
+  def run(self):
+    app = st.sidebar.selectbox('Navigation',self.apps,format_func = lambda app : app['title'])
+    app['function']()
+    
