ab29cefd4c225cfb99b4513d9b91346455e253e2
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-04-21 21:12:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,6 +102,8 @@ if user_input:
   
   def million(value):
     if(value <1000):
+      if('.0' in str(k)):
+        return (str(round(value)))
       return (('{:.2f}'.format(value)))
     if(value<1000000 and value>=1000):
       k=value/1000
