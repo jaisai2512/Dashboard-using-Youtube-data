bdfc0f086f92956811106c37bac0ed58d69b1635
jaisai2512 <104774963+jaisai2512@users.noreply.github.com>
2023-04-21 21:01:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,17 +16,26 @@ st.sidebar.header('Input here')
 user_input = st.sidebar.text_input("Enter the channel code:")
 
 
+
+
 if user_input:
 
   api_key ="AIzaSyA8gdudsfXk1YcEDS42CyeSiD7zXA1KWhI"
   youtube = build('youtube','v3',developerKey=api_key)
+  search_response = youtube.search().list(
+    q=user_input,
+    type='channel',
+    part='id'
+  ).execute()
+
+  channel_id = search_response['items'][0]['id']['channelId']
   
   
   def get_channel_stats(youtube,channel_id):
     request = youtube.channels().list(part="snippet,contentDetails,statistics",id=channel_id)
     response = request.execute()
     return response
-  data=get_channel_stats(youtube,user_input)
+  data=get_channel_stats(youtube,channel_id)
   data_dict=[]
   for i in data['items']:
     data_dict.append(dict(Channel_Name=i['snippet']['title'],Description=i['snippet']['description'],subcribers=i['statistics']['subscriberCount'],videoCount=i['statistics']['videoCount'] ,viewCount=i['statistics']['viewCount'],uploads=i['contentDetails']['relatedPlaylists']['uploads'],thumbnail=i['snippet']['thumbnails']['default']['url'],localized=i['snippet']['country']))
